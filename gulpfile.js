68b04d203649f7abb64e3f9a3f2f78a58469f6db
Marc Arndt <marc.arndt@masimi.de>
2017-01-09 14:26:22 +0100

    - remove the cleanup process from the gulp file because it doesn't work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var clean = require('gulp-clean');
 
 var inlineResources = require('./scripts/gulp/inline-resources');
 
-gulp.task('copy-and-inline-resources', copyHtml);
+gulp.task('copy-and-inline-resource', copyHtml);
 
 function copyHtml() {
   gulp.src('src/components/**/*.html')
@@ -31,21 +30,4 @@ function inlineResource() {
   inlineResources('./dist/components');
 }
 
-gulp.task('cleanup-unused-resources', [deleteHtml, deleteCss, deleteLess]);
-
-function deleteHtml() {
-  return gulp.src('./dist/components/**/*.html', { read: false })
-    .pipe(clean());
-}
-
-function deleteCss() {
-  return gulp.src('./dist/components/**/*.css', { read: false })
-    .pipe(clean());
-}
-
-function deleteLess() {
-  return gulp.src('./dist/components/**/*.less', { read: false })
-    .pipe(clean());
-}
-
-gulp.task('default', ['copy-and-inline-resources', 'cleanup-unused-resources']);
+gulp.task('default', ['copy-and-inline-resource']);
