86dfdab138beb89d9af4465cb245d9f996cbd4c1
Marc Arndt <marc.arndt@masimi.de>
2017-02-20 22:36:31 +0100

    - replacing the rimraf call inside the script in package.json through a gulp cleanup task that calls rimraf to delete both the tmp and dist folders - adding a few comments to the gulp file - adding a prepublish step to the package.json file to call npm run build before publishing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@ import ngc from 'gulp-ngc';
 import less from 'gulp-less';
 import inline from 'gulp-inline-ng2-template';
 
+import rimraf from 'rimraf';
+
 import fs from 'fs';
 
 function lessProcessor(path, ext, file, cb) {
@@ -13,17 +15,34 @@ function lessProcessor(path, ext, file, cb) {
   }
 }
 
-gulp.task('move-to-tmp', () => {
+/**
+ * delete the tmp and dist folders
+ */
+gulp.task('cleanup', (cb) => {
+  rimraf.sync('tmp');
+  rimraf('dist', cb);
+});
+
+/**
+ * move all files from the src folder to the tmp folder
+ */
+gulp.task('move-to-tmp', ['cleanup'], () => {
   return gulp.src(["src/**/*"])
     .pipe(gulp.dest('tmp'));
 });
 
-gulp.task('less', () => {
+/**
+ * compile all less files to css files in the tmp folder
+ */
+gulp.task('less', ['cleanup'], () => {
   return gulp.src('./src/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('tmp'));
 });
 
+/**
+ * inline both the html and stylesheets inside the angular 2 typescript template files inside the tmp folder
+ */
 gulp.task('inline', ['less', 'move-to-tmp'], () => {
   return gulp.src(['tmp/**/*.ts'])
     .pipe(inline({
@@ -33,8 +52,12 @@ gulp.task('inline', ['less', 'move-to-tmp'], () => {
     .pipe(gulp.dest('tmp'));
 });
 
+/**
+ * compile the typescript files from the tmp folder to javascript files and save the resulting files
+ * inside the dist folder
+ */
 gulp.task('compile', ['inline'], () => {
   return ngc('tmp/tsconfig.aot.json');
 });
 
-gulp.task('default', ['move-to-tmp', 'less', 'compile']);
+gulp.task('default', ['cleanup', 'move-to-tmp', 'less', 'inline', 'compile']);
