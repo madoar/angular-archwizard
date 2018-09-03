e613991051ca965bbcebb0c85a9b18d9c05d4920
Evgeny Arshinov <earshinov@gmail.com>
2018-09-03 17:32:18 +0300

    Provide ability to define step navigation symbol in the form of Angular template.
    For example:
    
    ```
    <div awWizardStep>
       <ng-template awWizardStepTitle>
           Address information
       </ng-template>
       <ng-template awWizardStepSymbol>
           <i class="fa fa-taxi"></i>
       </ng-template>
    </div>
    ```
    
    TODO:
    - Document in the main README.md
    - Add an example in the [demo app](https://github.com/madoar/angular-archwizard-demo/)

---
+++ src/directives/wizard-step-symbol.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, TemplateRef} from '@angular/core';
+
+/**
+ * The `awWizardStepSymbol` directive can be used as an alternative to the `navigationSymbol` input of a [[WizardStep]]
+ * to define the step symbol inside the navigation bar.  This way step symbol may contain arbitrary content.
+ *
+ * ### Syntax
+ *
+ * ```html
+ * <ng-template awWizardStepSymbol>
+ *     ...
+ * </ng-template>
+ * ```
+ */
+@Directive({
+  selector: 'ng-template[awStepSymbol], ng-template[awWizardStepSymbol]'
+})
+export class WizardStepSymbolDirective {
+  /**
+   * Constructor
+   *
+   * @param templateRef A reference to the content of the `ng-template` that contains this [[WizardStepSymbolDirective]]
+   */
+  constructor(public templateRef: TemplateRef<any>) { }
+}
--- src/directives/wizard-step.directive.ts
+++ src/directives/wizard-step.directive.ts
@@ -6,7 +6,7 @@ import {WizardStep} from '../util/wizard-step.interface';
  *
  * ### Syntax
  *
- * With `stepTitle` input:
+ * With `stepTitle` and `navigationSymbol` inputs:
  *
  * ```html
  * <div awWizardStep [stepTitle]="step title" [navigationSymbol]="{ symbol: 'symbol', fontFamily: 'font-family' }"
@@ -15,13 +15,15 @@ import {WizardStep} from '../util/wizard-step.interface';
  * </div>
  * ```
  *
- * With `awWizardStepTitle` directive:
+ * With `awWizardStepTitle` and `awWizardStepSymbol` directives:
  *
  * ```html
- * <div awWizardStep [navigationSymbol]="{ symbol: 'symbol', fontFamily: 'font-family' }"
- *    [canExit]="deciding function" (stepEnter)="enter function" (stepExit)="exit function">
+ * <div awWizardStep [canExit]="deciding function" (stepEnter)="enter function" (stepExit)="exit function">
  *    <ng-template awWizardStepTitle>
  *        step title
+ *    </ng-template>
+ *    <ng-template awWizardStepSymbol>
+ *        symbol
  *    </ng-template>
  *    ...
  * </div>
@@ -29,7 +31,7 @@ import {WizardStep} from '../util/wizard-step.interface';
  *
  * ### Example
  *
- * With `stepTitle` input:
+ * With `stepTitle` and `navigationSymbol` inputs:
  *
  * ```html
  * <div awWizardStep stepTitle="Address information" [navigationSymbol]="{ symbol: '&#xf1ba;', fontFamily: 'FontAwesome' }">
@@ -37,12 +39,15 @@ import {WizardStep} from '../util/wizard-step.interface';
  * </div>
  * ```
  *
- * With `awWizardStepTitle` directive:
+ * With `awWizardStepTitle` and `awWizardStepSymbol` directives:
  *
  * ```html
- * <div awWizardStep [navigationSymbol]="{ symbol: '&#xf1ba;', fontFamily: 'FontAwesome' }">
+ * <div awWizardStep>
  *    <ng-template awWizardStepTitle>
  *        Address information
+ *    </ng-template>
+ *    <ng-template awWizardStepSymbol>
+ *        <i class="fa fa-taxi"></i>
  *    </ng-template>
  * </div>
  * ```
