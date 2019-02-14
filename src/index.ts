4851e394abd5f34b25fd2d7d3cf74cafc2944a3b
Zachary Hale <zachary.hale88@gmail.com>
2019-02-14 11:06:15 -0500

    Removing barrel export and explicitly exporting each component in base index.ts.  Barrel exports of components cause @ViewChild(<className>) to give undefined in AOT builds (#184)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export * from './lib/components';
+export { WizardCompletionStepComponent } from './lib/components/wizard-completion-step.component';
+export { WizardNavigationBarComponent } from './lib/components/wizard-navigation-bar.component';
+export { WizardStepComponent } from './lib/components/wizard-step.component';
+export { WizardComponent } from './lib/components/wizard.component';
+
 export * from './lib/directives';
 export * from './lib/navigation';
 export * from './lib/util';
