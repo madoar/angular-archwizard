a8a0719ff481e918493b48aba32000ab9a4615cd
Marc Arndt <marc.arndt@masimi.de>
2019-02-14 17:38:26 +0100

    - improve imports in archwizard module

--- src/lib/archwizard.module.ts
+++ src/lib/archwizard.module.ts
@@ -1,22 +1,20 @@
 import {CommonModule} from '@angular/common';
 import {ModuleWithProviders, NgModule} from '@angular/core';
 
-import {WizardComponent} from './components/wizard.component';
-import {WizardNavigationBarComponent} from './components/wizard-navigation-bar.component';
-import {WizardStepComponent} from './components/wizard-step.component';
-import {WizardCompletionStepComponent} from './components/wizard-completion-step.component';
-
-import {NextStepDirective} from './directives/next-step.directive';
-import {PreviousStepDirective} from './directives/previous-step.directive';
-import {OptionalStepDirective} from './directives/optional-step.directive';
-import {GoToStepDirective} from './directives/go-to-step.directive';
-import {WizardStepSymbolDirective} from './directives/wizard-step-symbol.directive';
-import {WizardStepTitleDirective} from './directives/wizard-step-title.directive';
-import {EnableBackLinksDirective} from './directives/enable-back-links.directive';
-import {WizardStepDirective} from './directives/wizard-step.directive';
-import {WizardCompletionStepDirective} from './directives/wizard-completion-step.directive';
-import {SelectedStepDirective} from './directives/selected-step.directive';
-import {ResetWizardDirective} from './directives/reset-wizard.directive';
+import {WizardCompletionStepComponent, WizardComponent, WizardNavigationBarComponent, WizardStepComponent} from './components';
+import {
+  EnableBackLinksDirective,
+  GoToStepDirective,
+  NextStepDirective,
+  OptionalStepDirective,
+  PreviousStepDirective,
+  ResetWizardDirective,
+  SelectedStepDirective,
+  WizardCompletionStepDirective,
+  WizardStepDirective,
+  WizardStepSymbolDirective,
+  WizardStepTitleDirective
+} from './directives';
 
 /**
  * The module defining all the content inside `angular-archwizard`
