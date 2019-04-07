b11dcbeae35e0234fa67675b6b9f66b746cc198a
Marc Arndt <marc.arndt@masimi.de>
2019-04-07 21:03:09 +0200

    - remove user configuration in the Dockerfile - add custom launcher to karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeBrowser'],
+    customLaunchers: {
+      ChromeBrowser: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
