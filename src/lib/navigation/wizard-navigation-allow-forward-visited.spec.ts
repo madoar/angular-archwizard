585b9b5be62e5ddb3a9f167c30659e9097e0c2be
Marc Arndt <marc.arndt@masimi.de>
2019-08-11 17:59:54 +0200

    - fix missing {static: false} in navigateForward = visited test

--- src/lib/navigation/wizard-navigation-allow-forward-visited.spec.ts
+++ src/lib/navigation/wizard-navigation-allow-forward-visited.spec.ts
@@ -21,7 +21,7 @@ import {checkWizardState, checkWizardNavigableSteps} from '../util/test-utils';
   `
 })
 class WizardTestComponent {
-  @ViewChild(WizardComponent)
+  @ViewChild(WizardComponent, {static: false})
   public wizard: WizardComponent;
 }
 
