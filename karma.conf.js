1e4983b6c7b919bfb3b90a88b3e3444a6f6f29c4
Marc Arndt <marc.arndt@masimi.de>
2019-10-20 21:13:27 +0200

    - cleanup the karma config file - add a configuration for running the tests of firefox - remove the custom chrome headless configuration

--- karma.conf.js
+++ karma.conf.js
@@ -6,30 +6,28 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-coverage-istanbul-reporter',
+      '@angular-devkit/build-angular/plugins/karma'
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ModifiedChromeHeadless'],
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
     customLaunchers: {
-      ModifiedChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless']
       }
     },
     singleRun: true
