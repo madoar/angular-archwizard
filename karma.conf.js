1ccf49221cdba84a3691275a2683f892e8d4a752
madoar <madoar@users.noreply.github.com>
2019-11-30 13:05:17 +0100

    Cleanup the Karma Configuration File (#245)
    - cleanup the karma config file
    - use karma-browser-detect plugin to detect all testable browsers
    - overhaul the .travis.yml file
    - add safari as testing browser
    - install chromium instead of chrome on gitpod

--- karma.conf.js
+++ karma.conf.js
@@ -4,32 +4,61 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'detectBrowsers'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-safari-launcher',
+      'karma-detect-browsers',
+      'karma-jasmine-html-reporter',
+      'karma-coverage-istanbul-reporter',
+      '@angular-devkit/build-angular/plugins/karma'
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80
+      }
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ModifiedChromeHeadless'],
     customLaunchers: {
-      ModifiedChromeHeadless: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
+      },
+      ChromiumHeadlessNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    detectBrowsers: {
+      // enable/disable, default is true
+      enabled: true,
+
+      // enable/disable phantomjs support, default is true
+      usePhantomJS: false,
+
+      // use headless mode, for browsers that support it, default is false
+      preferHeadless: true,
+
+      postDetection: function (availableBrowsers) {
+        if (availableBrowsers.includes('ChromeHeadless')) {
+          const index = availableBrowsers.indexOf('ChromeHeadless');
+
+          availableBrowsers[index] = 'ChromeHeadlessNoSandbox';
+        }
+
+        if (availableBrowsers.includes('ChromiumHeadless')) {
+          const index = availableBrowsers.indexOf('ChromiumHeadless');
+
+          availableBrowsers[index] = 'ChromiumHeadlessNoSandbox';
+        }
+
+        return availableBrowsers;
       }
     },
     singleRun: true
