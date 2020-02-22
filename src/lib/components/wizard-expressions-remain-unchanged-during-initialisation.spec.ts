223c1ab329c48ea08ccfb4a313a661dfc72a97b6
Marc Arndt <marc.arndt@masimi.de>
2020-02-22 10:47:43 +0100

    - simplify new expressions remain unchanged during initialisation test

--- src/lib/components/wizard-expressions-remain-unchanged-during-initialisation.spec.ts
+++ src/lib/components/wizard-expressions-remain-unchanged-during-initialisation.spec.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ArchwizardModule } from '../archwizard.module';
 import { WizardComponent } from './wizard.component';
@@ -6,16 +6,9 @@ import { WizardComponent } from './wizard.component';
 @Component({
   selector: 'aw-test-wizard',
   template: `
-    <aw-wizard [defaultStepIndex]="0">
-      <aw-wizard-step stepTitle='Steptitle 1' (stepEnter)="enterFirstStep()">
-        Step 1
+    <aw-wizard>
+      <aw-wizard-step stepTitle='Single Steptitle' (stepEnter)="enterStep()">
         {{counter}}
-      </aw-wizard-step>
-      <aw-wizard-step stepTitle='Steptitle 2'>
-        Step 2
-      </aw-wizard-step>
-      <aw-wizard-step stepTitle='Steptitle 3'>
-        Step 3
       </aw-wizard-step>
     </aw-wizard>
   `
@@ -26,7 +19,7 @@ class WizardTestComponent {
   @ViewChild(WizardComponent)
   public wizard: WizardComponent;
 
-  public enterFirstStep(): void {
+  public enterStep(): void {
     this.counter++;
   }
 }
