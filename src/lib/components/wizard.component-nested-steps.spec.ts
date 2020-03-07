a3ad9a1ef3f90db7a5f6adc6981b466b3cc4fbe3
madoar <madoar@users.noreply.github.com>
2020-03-07 14:40:16 +0100

    Improve the Tests for nested wizard steps (#272)
    - add a more fitting description
    - add some content to the steps in the wizard under test
    - add some checks that compare the actual wizard steps against the detected steps

--- src/lib/components/wizard.component-nested-steps.spec.ts
+++ src/lib/components/wizard.component-nested-steps.spec.ts
@@ -2,30 +2,47 @@ import { Component, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { ArchwizardModule } from '../archwizard.module';
 import { WizardComponent } from './wizard.component';
+import { WizardStep } from '../util/wizard-step.interface';
 
 @Component({
   selector: 'aw-test-wizard',
   template: `
     <aw-wizard>
       <div>
-        <aw-wizard-step></aw-wizard-step>
+        <aw-wizard-step #firstStep stepTitle='Steptitle 1'>
+          Step 1
+        </aw-wizard-step>
       </div>
       <div>
-        <aw-wizard-step></aw-wizard-step>
+        <aw-wizard-step #secondStep stepTitle='Steptitle 2'>
+          Step 2
+        </aw-wizard-step>
       </div>
       <div>
         <div>
-          <aw-wizard-step></aw-wizard-step>
+          <aw-wizard-step #thirdStep stepTitle='Steptitle 3'>
+            Step 3
+          </aw-wizard-step>
         </div>
       </div>
     </aw-wizard>
   `
 })
 class WizardTestComponent {
-  @ViewChild(WizardComponent) public wizard: WizardComponent;
+  @ViewChild(WizardComponent)
+  public wizard: WizardComponent;
+
+  @ViewChild('firstStep')
+  public first: WizardStep;
+
+  @ViewChild('secondStep')
+  public second: WizardStep;
+
+  @ViewChild('thirdStep')
+  public third: WizardStep;
 }
 
-describe('WizardComponent', () => {
+describe('WizardComponent with nested steps', () => {
   let wizardTestFixture: ComponentFixture<WizardTestComponent>;
 
   let wizardTest: WizardTestComponent;
@@ -57,5 +74,9 @@ describe('WizardComponent', () => {
 
   it('should contain 3 steps', () => {
     expect(wizard.wizardSteps.length).toBe(3);
+
+    expect(wizardTest.first).toBe(wizard.wizardSteps[0]);
+    expect(wizardTest.second).toBe(wizard.wizardSteps[1]);
+    expect(wizardTest.third).toBe(wizard.wizardSteps[2]);
   });
 });
